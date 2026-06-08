3836858378a91c635cba377fb4579e8612849614
torzdf <36920800+torzdf@users.noreply.github.com>
2026-06-08 12:04:27 +0100

    Hide warnings in custom layers when JIT Tracing

--- lib/model/layers.py
+++ lib/model/layers.py
@@ -3,11 +3,13 @@
 from __future__ import annotations
 
 import logging
+import math
+import warnings
 
-import numpy as np
 import torch
 from torch import nn
 from torch.nn import functional as F
+from torch import jit
 
 from lib.logger import parse_class_init
 from lib.utils import get_module_objects
@@ -35,8 +37,16 @@ class SamePad2d(nn.Module):
         """Better info for debug output"""
         return f"{self.__class__.__name__}(kernel_size={self.kernel}, stride={self.stride})"
 
+    def _pad(self, inputs: torch.Tensor) -> torch.Tensor:
+        """Perform asymmetric padding to the input tensor"""
+        height, width = inputs.shape[-2:]
+        pad_h = max((math.ceil(height / self.stride) - 1) * self.stride + self.kernel - height, 0)
+        pad_w = max((math.ceil(width / self.stride) - 1) * self.stride + self.kernel - width, 0)
+        return F.pad(inputs, (pad_w // 2, pad_w - pad_w // 2,
+                              pad_h // 2, pad_h - pad_h // 2))
+
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        """Apply asymmetric padding to the input tensor
+        """Forward pass through the legacy padding layer
 
         Parameters
         ----------
@@ -47,13 +57,13 @@ class SamePad2d(nn.Module):
         -------
         The padded tensor
         """
-        height, width = inputs.shape[-2:]
-        pad_h = int(max((np.ceil(height / self.stride) - 1) * self.stride + self.kernel - height,
-                        0))
-        pad_w = int(max((np.ceil(width / self.stride) - 1) * self.stride + self.kernel - width,
-                        0))
-        return F.pad(inputs, (pad_w // 2, pad_w - pad_w // 2,
-                              pad_h // 2, pad_h - pad_h // 2))
+        if jit.is_tracing():  # Hide nasty constant warnings when JIT tracing
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore",
+                                        message="Converting a tensor to a Python",
+                                        category=jit.TracerWarning)
+                return self._pad(inputs)
+        return self._pad(inputs)
 
 
 __all__ = get_module_objects(__name__)
