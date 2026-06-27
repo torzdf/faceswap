b9d5ce3668425c450221956745dd3328bedc0780
torzdf <36920800+torzdf@users.noreply.github.com>
2026-06-27 13:55:17 +0100

    Remove legacy layers from layers.py

--- lib/model/layers.py
+++ lib/model/layers.py
@@ -3,124 +3,18 @@
 from __future__ import annotations
 
 import logging
-import math
 import typing as T
-import warnings
 
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torch import jit
 
 from lib.logger import parse_class_init
 from lib.utils import get_module_objects
 
+from .layers_legacy import SamePad2d
+
 logger = logging.getLogger(__name__)
-
-
-class InstanceNormLegacy(nn.Module):
-    """A stripped down instance normalization that mimics Keras' epsilon, beta and gamma
-    implementation. Don't use for new models. Use nn.InstanceNorm2d instead
-
-    Parameters
-    ----------
-    eps
-        Small float added to variance to avoid dividing by zero. Default: `1e-3`
-    """
-    def __init__(self, eps: float = 1e-3) -> None:
-        logger.debug(parse_class_init(locals()))
-        super().__init__()
-        self.eps = eps
-        self.weight = nn.Parameter(torch.ones(1))  # gamma
-        self.bias = nn.Parameter(torch.zeros(1))  # beta
-
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        """Forward pass through the legacy keras instance normalization layer
-
-        Parameters
-        ----------
-        inputs
-            The tensor to normalize
-
-        Returns
-        -------
-            The normalized tensor
-        """
-        reduction_dims = tuple(range(1, len(inputs.shape)))
-        mean = inputs.mean(dim=reduction_dims, keepdim=True)
-        stdev = inputs.std(dim=reduction_dims, unbiased=True, keepdim=True) + self.eps
-        normed = (inputs - mean) / stdev
-        return normed * self.weight + self.bias
-
-
-class SamePad2d(nn.Module):
-    """Asymmetric padding to replicate Keras' padding='same' for backwards compatibility. This
-    should not be used to new models. It exists purely to enable bit-accurate porting of Tensorflow
-    built models into Torch
-
-    Parameters
-    ----------
-    kernel_size
-        The size of the kernel for the following convolution
-    stride
-        The size of the stride for the following convolution
-    padding_mode
-        The type of padding to apply. Default: `constant`
-    """
-    def __init__(self,
-                 kernel_size: int,
-                 stride: int,
-                 mode: T.Literal["constant", "reflect"] = "constant") -> None:
-        logger.debug(parse_class_init(locals()))
-        super().__init__()
-        self.kernel = kernel_size
-        self.stride = stride
-        self.mode = mode
-
-    def __repr__(self) -> str:
-        """Better info for debug output"""
-        return (f"{self.__class__.__name__}(kernel_size={self.kernel}, "
-                f"stride={self.stride}, mode={self.mode})")
-
-    def _pad(self, inputs: torch.Tensor) -> torch.Tensor:
-        """Perform asymmetric padding to the input tensor
-
-        Parameters
-        ----------
-        inputs
-            The input to the padding layer
-
-        Returns
-        -------
-        The padded input tensor
-        """
-        height, width = inputs.shape[-2:]
-        pad_h = max((math.ceil(height / self.stride) - 1) * self.stride + self.kernel - height, 0)
-        pad_w = max((math.ceil(width / self.stride) - 1) * self.stride + self.kernel - width, 0)
-        return F.pad(inputs,
-                     (pad_w // 2, pad_w - pad_w // 2,
-                      pad_h // 2, pad_h - pad_h // 2),
-                     mode=self.mode)
-
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        """Forward pass through the legacy padding layer
-
-        Parameters
-        ----------
-        inputs
-            The tensor to be padded
-
-        Returns
-        -------
-        The padded tensor
-        """
-        if jit.is_tracing():  # Hide nasty constant warnings when JIT tracing
-            with warnings.catch_warnings():
-                warnings.filterwarnings("ignore",
-                                        message="Converting a tensor to a Python",
-                                        category=jit.TracerWarning)
-                return self._pad(inputs)
-        return self._pad(inputs)
 
 
 class SeparableConv2d(nn.Module):
@@ -201,41 +95,6 @@ class SeparableConv2d(nn.Module):
         x = self.depthwise(x)
         x = self.pointwise(x)
         return x
-
-
-class UpSampling2dLegacy(nn.Module):
-    """Upsampling layer to match Keras behavior. Do not use this for new models
-
-    Parameters
-    ----------
-    size
-        The upsampling factors for rows and columns. Default: (2, 2)
-    interpolation
-        The interpolation to use. Default: "nearest"
-    """
-    def __init__(self,
-                 size: int | tuple[int, int] = (2, 2),
-                 interpolation: T.Literal["bicubic", "bilinear", "nearest"] = "nearest") -> None:
-        logger.debug(parse_class_init(locals()))
-        super().__init__()
-        self.factor = (size, size) if isinstance(size, int) else size
-        self.mode = interpolation
-        self.align_corners = False if interpolation in ("bicubic", "bilinear") else None
-
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        """Call the Upsampling2D Layer
-
-        Parameters
-        ----------
-        inputs
-            The input tensor to be upsampled
-
-        Returns
-        -------
-        The upsampled tensor
-        """
-        size = (inputs.shape[-2] * self.factor[0], inputs.shape[-1] * self.factor[1])
-        return F.interpolate(inputs, size=size, mode=self.mode, align_corners=self.align_corners)
 
 
 __all__ = get_module_objects(__name__)
